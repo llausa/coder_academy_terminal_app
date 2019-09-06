ac888a24872a7a6e11e187b1fc8cebdac4a1fc94
Santiago <santiago.chamon@gmail.com>
2019-09-06 14:55:07 +1000

    require_relative paths fixed

--- tests/todo_list_test.rb
+++ tests/todo_list_test.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require 'test/unit' # https://www.rubydoc.info/gems/test-unit/2.3.1/Test/Unit
-require_relative 'src/todo_list' # importing ToDoList class
-require_relative 'src/todo_item' # importing ToDoItem class
+require_relative '../src/todo_list' # importing ToDoList class
+require_relative '../src/todo_item' # importing ToDoItem class
 
 # Setup tests for the ToDo_List class
 class ToDoListTest < Test::Unit::TestCase
