88f4d6faac1c54465061dd9eea6c6b5688d2340f
Santiago <santiago.chamon@gmail.com>
2019-09-06 13:16:23 +1000

    require-relative used in automated_test_script

--- src/automated_test_script.rb
+++ src/automated_test_script.rb
@@ -2,6 +2,6 @@
 
 # by running this file, you run all the tests inside the files below, testing the main functionality of the app
 
-require '/Users/Santiago/desktop/ca_workbook/shtda1/tests/todo_item_test'
-require '/Users/Santiago/desktop/ca_workbook/shtda1/tests/todo_list_test'
-require '/Users/Santiago/desktop/ca_workbook/shtda1/tests/user_test'
+require-relative 'tests/todo_item_test'
+require-relative 'tests/todo_list_test'
+require-relative 'tests/user_test'
